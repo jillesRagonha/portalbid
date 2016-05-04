ca954dc452c5340fd4ec875b48032838f92f2348
jilles_ragonha <jilles_ragonha@bitbucket.org>
2016-05-04 16:08:44 -0300

    alterando componentes

--- src/main/java/br/com/agilles/portalbid/controller/LojistaMB.java
+++ src/main/java/br/com/agilles/portalbid/controller/LojistaMB.java
@@ -1,9 +1,7 @@
 package br.com.agilles.portalbid.controller;
 
 import br.com.agilles.portalbid.dao.LojistaDAO;
-import br.com.agilles.portalbid.models.Endereco;
 import br.com.agilles.portalbid.models.Lojista;
-import br.com.agilles.portalbid.models.Usuario;
 
 import javax.faces.bean.ManagedBean;
 
@@ -12,26 +10,10 @@ import javax.faces.bean.ManagedBean;
  */
 @ManagedBean
 public class LojistaMB {
-    private Usuario usuario = new Usuario();
-    private Endereco endereco = new Endereco();
+
     private Lojista lojista = new Lojista();
     private LojistaDAO dao = new LojistaDAO();
 
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
 
     public Lojista getLojista() {
         return lojista;
--- src/main/java/br/com/agilles/portalbid/models/Endereco.java
+++ src/main/java/br/com/agilles/portalbid/models/Endereco.java
@@ -15,6 +15,42 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cep;
+    private String cidade;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 
     public Integer getId() {
         return id;
